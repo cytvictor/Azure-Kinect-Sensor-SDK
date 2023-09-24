6a0f0abc8ccad203ba498cb75dd9c8579da56ab8
Yongting <yongting@nyu.edu>
2023-09-24 12:24:38 +0000

    feat(LiveVV): extract PCD from mkv for all frames

--- examples/transformation/main.cpp
+++ examples/transformation/main.cpp
@@ -4,6 +4,7 @@
 #include <k4a/k4a.h>
 #include <k4arecord/playback.h>
 #include <string>
+#include <sstream>
 #include "transformation_helpers.h"
 #include "turbojpeg.h"
 
@@ -432,11 +433,162 @@ Exit:
     }
     return returnCode;
 }
+// Timestamp in milliseconds. Defaults to 1 sec as the first couple frames don't contain color
+static int playback_all(char *input_path, std::string output_filename_prefix = "output")
+{
+    int returnCode = 1, frameNo = 0;
+    k4a_playback_t playback = NULL;
+    k4a_calibration_t calibration;
+    k4a_transformation_t transformation = NULL;
+    k4a_capture_t capture = NULL;
+    k4a_image_t depth_image = NULL;
+    k4a_image_t color_image = NULL;
+    k4a_image_t uncompressed_color_image = NULL;
+
+    k4a_result_t result;
+    k4a_stream_result_t stream_result;
+
+    // Open recording
+    result = k4a_playback_open(input_path, &playback);
+    if (result != K4A_RESULT_SUCCEEDED || playback == NULL)
+    {
+        printf("Failed to open recording %s\n", input_path);
+        continue;
+        goto Exit;
+    }
+
+    while (true) {
+        stream_result = k4a_playback_get_next_capture(playback, &capture);
+        if (stream_result != K4A_STREAM_RESULT_SUCCEEDED || capture == NULL)
+        {
+            printf("Failed to fetch frame\n");
+            continue;
+            goto Exit;
+        }
+
+        if (K4A_RESULT_SUCCEEDED != k4a_playback_get_calibration(playback, &calibration))
+        {
+            printf("Failed to get calibration\n");
+            continue;
+            goto Exit;
+        }
+
+        transformation = k4a_transformation_create(&calibration);
+
+        // Fetch frame
+        depth_image = k4a_capture_get_depth_image(capture);
+        if (depth_image == 0)
+        {
+            printf("Failed to get depth image from capture\n");
+            continue;
+            goto Exit;
+        }
+
+        color_image = k4a_capture_get_color_image(capture);
+        if (color_image == 0)
+        {
+            printf("Failed to get color image from capture\n");
+            continue;
+            // goto Exit;
+        }
+
+        ///////////////////////////////
+        // Convert color frame from mjpeg to bgra
+        k4a_image_format_t format;
+        format = k4a_image_get_format(color_image);
+        if (format != K4A_IMAGE_FORMAT_COLOR_MJPG)
+        {
+            printf("Color format not supported. Please use MJPEG\n");
+            continue;
+            goto Exit;
+        }
+
+        int color_width, color_height;
+        color_width = k4a_image_get_width_pixels(color_image);
+        color_height = k4a_image_get_height_pixels(color_image);
+
+        if (K4A_RESULT_SUCCEEDED != k4a_image_create(K4A_IMAGE_FORMAT_COLOR_BGRA32,
+                                                    color_width,
+                                                    color_height,
+                                                    color_width * 4 * (int)sizeof(uint8_t),
+                                                    &uncompressed_color_image))
+        {
+            printf("Failed to create image buffer\n");
+            continue;
+            goto Exit;
+        }
+
+        tjhandle tjHandle;
+        tjHandle = tjInitDecompress();
+        if (tjDecompress2(tjHandle,
+                        k4a_image_get_buffer(color_image),
+                        static_cast<unsigned long>(k4a_image_get_size(color_image)),
+                        k4a_image_get_buffer(uncompressed_color_image),
+                        color_width,
+                        0, // pitch
+                        color_height,
+                        TJPF_BGRA,
+                        TJFLAG_FASTDCT | TJFLAG_FASTUPSAMPLE) != 0)
+        {
+            printf("Failed to decompress color frame\n");
+            if (tjDestroy(tjHandle))
+            {
+                printf("Failed to destroy turboJPEG handle\n");
+            }
+            continue;
+            goto Exit;
+        }
+        if (tjDestroy(tjHandle))
+        {
+            printf("Failed to destroy turboJPEG handle\n");
+        }
+        ///////////////////////////////
+
+        // Compute color point cloud by warping depth image into color camera geometry
+        std::stringstream final_filename;
+        final_filename << output_filename_prefix << "-" << frameNo++ << ".ply";
+        if (point_cloud_depth_to_color(transformation, depth_image, uncompressed_color_image, final_filename.str()) == false)
+        {
+            printf("Failed to transform depth to color\n");
+            continue;
+            goto Exit;
+        }
+        returnCode = 0;
+    }
+
+Exit:
+    if (playback != NULL)
+    {
+        k4a_playback_close(playback);
+    }
+    if (depth_image != NULL)
+    {
+        k4a_image_release(depth_image);
+    }
+    if (color_image != NULL)
+    {
+        k4a_image_release(color_image);
+    }
+    if (uncompressed_color_image != NULL)
+    {
+        k4a_image_release(uncompressed_color_image);
+    }
+    if (capture != NULL)
+    {
+        k4a_capture_release(capture);
+    }
+    if (transformation != NULL)
+    {
+        k4a_transformation_destroy(transformation);
+    }
+    return returnCode;
+}
 
 static void print_usage()
 {
     printf("Usage: transformation_example capture <output_directory> [device_id]\n");
     printf("Usage: transformation_example playback <filename.mkv> [timestamp (ms)] [output_file]\n");
+    printf("Usage: transformation_example playback-all <filename.mkv> [output_file_prefix]\n");
 }
 
 int main(int argc, char **argv)
@@ -478,6 +630,17 @@ int main(int argc, char **argv)
             else if (argc == 5)
             {
                 returnCode = playback(argv[2], atoi(argv[3]), argv[4]);
+            }
+            else
+            {
+                print_usage();
+            }
+        }
+        else if (mode == "playback-all")
+        {
+            if (argc == 4)
+            {
+                returnCode = playback_all(argv[2], argv[3]);
             }
             else
             {
